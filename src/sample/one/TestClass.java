5b73b64d69be4e51b1517838c408fafcfc219adb
niranjana2108 <niranjana2108@gmail.com>
2023-05-08 19:03:09 +0530

    Initial commit

---
+++ src/sample/one/TestClass.java
@@ -0,0 +1,28 @@
+package sample.one;
+
+public class TestClass {
+
+    /**
+     * Documentation comment
+     * This method is used to
+     *
+     * author
+     * returntype void
+     * @param args
+     */
+
+    public static void main(String[] args) {
+
+        //this is single line comment
+
+   /*
+   This is multiline comment
+        This is test
+        This is test
+        This is test
+        */
+
+
+        System.out.println("Hello World!!!");
+    }
+}
